09de46c61f621f4f8ff30a196ba6727674224d6d
Jose Gonzales <118575003+JoseGonzaless@users.noreply.github.com>
2024-10-28 13:15:57 -0500

    Init project and search box

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,33 @@
+import { Stack } from 'expo-router';
+import { useState } from 'react';
+import {Text, View, TextInput, Pressable } from 'react-native';
+
+export default function Home() {
+  const [search, setSearch] = useState('');
+
+  const performSearch = () => {
+    console.warn('Search', search);
+
+    //save this search in database
+
+    //scrape amazon for this query
+  };
+
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Search' }} />
+      <View className='flex-row gap-3 p-3'>
+        <TextInput 
+          value={search}
+          onChangeText={setSearch}
+          placeholder='Search for a item' 
+          className='flex-1 rounded border bg-gray-200 p-3'
+        />
+        
+        <Pressable onPress={performSearch} className='rounded bg-teal-500 p-3'>
+          <Text>Search</Text>
+        </Pressable>
+      </View>
+    </>
+  );
+}
